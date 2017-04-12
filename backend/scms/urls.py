3b6cf702d604d6eca9fb6004712fb04473f89bd7
Sean Mc Allister <sean@gmx.org>
2017-04-12 08:39:00 +0200

    fake the static pages for the time being

--- backend/scms/urls.py
+++ backend/scms/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import debugView
+from .views import debugView, CollectionView
 
 urlpatterns = [
+    url(r'(?P<collection_name>\w+)/$', CollectionView.as_view(), name='collection_detail'),
     url(r'^(?P<path>.*/)?', debugView)
 ]
--- backend/scms/views.py
+++ backend/scms/views.py
@@ -1,6 +1,7 @@
 import os
 import json
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 CONTENT_PATH = os.path.join(
     os.path.dirname(__file__),
@@ -24,3 +25,17 @@ def populate_menu(root=CONTENT_PATH):
     for dir, dirs, files in os.walk(root):
         content.append((os.path.relpath(dir, start=root), dirs, files))
     return content
+
+
+class CollectionView(TemplateView):
+
+    def get_template_names(self):
+        return [
+            'scms/collections/%s.html' % self.kwargs.get('collection_name'),
+            'scms/collections/default.html'
+        ]
+
+    def get_context_data(self, **kwargs):
+        return {
+            'collection': self.kwargs.get('collection_name')
+        }

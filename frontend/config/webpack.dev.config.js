f103ee859831075f3566701023e29b3105432402
Sean Mc Allister <sean@gmx.org>
2017-04-13 21:35:02 +0200

    prettified

--- frontend/config/webpack.dev.config.js
+++ frontend/config/webpack.dev.config.js
@@ -1,29 +1,27 @@
-let path = require('path');
-let webpack = require('webpack');
-let BundleTracker = require('webpack-bundle-tracker');
-let baseConfig = require('./webpack.base.config');
+let path = require("path");
+let webpack = require("webpack");
+let BundleTracker = require("webpack-bundle-tracker");
+let baseConfig = require("./webpack.base.config");
 
-
-module.exports = (opts) => {
-
-  const {CDN_PATH, PROJECT_ROOT} = opts,
-      config = baseConfig(opts),
-      output_path = path.resolve(PROJECT_ROOT, 'build/');
+module.exports = opts => {
+  const { CDN_PATH, PROJECT_ROOT } = opts,
+    config = baseConfig(opts),
+    output_path = path.resolve(PROJECT_ROOT, "build/");
 
   return {
     ...config,
-    devtool: 'source-map',
+    devtool: "source-map",
     output: {
-        ...config.output,
-        path: output_path,
-        publicPath: '/static/wp/',
+      ...config.output,
+      path: output_path,
+      publicPath: "/static/wp/"
     },
     plugins: [
       ...config.plugins,
       // local bundle stats file
       new BundleTracker({
         path: output_path,
-        filename: 'webpack-stats-development.json'
+        filename: "webpack-stats-development.json"
       }),
       new webpack.NoEmitOnErrorsPlugin(),
       new webpack.NamedModulesPlugin()
--- frontend/config/webpack.dev.hmr.config.js
+++ frontend/config/webpack.dev.hmr.config.js
@@ -1,30 +1,22 @@
-let webpack = require('webpack');
-let baseConfig = require('./webpack.dev.config.js');
+let webpack = require("webpack");
+let baseConfig = require("./webpack.dev.config.js");
 
-module.exports = (opts) => {
-
+module.exports = opts => {
   let config = baseConfig(opts);
 
   let entries = {};
 
-  Object.keys(config.entry).forEach((key) => {
-    entries[key] = [
-        'react-hot-loader/patch',
-        ...config.entry[key]
-    ]
+  Object.keys(config.entry).forEach(key => {
+    entries[key] = ["react-hot-loader/patch", ...config.entry[key]];
   });
 
   return {
     ...config,
     entry: entries,
-    plugins: [
-        ...config.plugins,
-        new webpack.HotModuleReplacementPlugin()
-    ],
+    plugins: [...config.plugins, new webpack.HotModuleReplacementPlugin()],
     output: {
       ...config.output,
-      publicPath: 'http://localhost:8080/bundles/'
-    },
-
+      publicPath: "http://localhost:8080/bundles/"
+    }
   };
 };

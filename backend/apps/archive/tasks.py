e172d2fb3e28a95653363bf71d2891bb4103378e
Sean Mc Allister <sean.mcallister@apa.at>
2017-10-17 15:47:32 +0200

    task decorator

--- backend/apps/archive/tasks.py
+++ backend/apps/archive/tasks.py
@@ -1,10 +1,10 @@
-# Create your tasks here
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
+from hav.celery import app
 
 from .operations.create import archive_file
 
-@shared_task
+
+@app.task
 def archive(filepath, media_id, user_id):
     return archive_file(filepath, media_id, user_id)
 

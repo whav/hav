51a09fa91f44c683e7156963b36c8927e7143863
Sean Mc Allister <sean@gmx.org>
2017-12-21 12:37:24 +0100

    ingest queue

--- backend/apps/ingest/admin.py
+++ backend/apps/ingest/admin.py
@@ -4,5 +4,13 @@ from .models import IngestQueue
 
 class IngestQAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'selection_length', 'created_at', 'created_by')
-
+    readonly_fields = (
+        'selection',
+        'created_by',
+        'expanded_selection',
+        'data',
+        'ingested',
+        'target',
+        'ready_for_ingestion'
+    )
 admin.site.register(IngestQueue, IngestQAdmin)

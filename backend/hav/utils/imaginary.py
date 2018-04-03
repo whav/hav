5d5d0e97db878b4a9859ba37a2d5813436552935
Sean Mc Allister <sean@gmx.org>
2018-04-03 14:34:30 +0200

    fooling around with env variables

--- backend/hav/utils/imaginary.py
+++ backend/hav/utils/imaginary.py
@@ -1,13 +1,20 @@
 from django.conf import settings
 from urllib.parse import urlencode, urlparse
 
+import base64
+import hashlib
+
+
+SECRET = 'TOPSECRET'
+
 def is_absolute(url):
     return bool(urlparse(url).netloc)
 
 def generate_imaginary_url(path):
     kwargs = {
         'width': 300,
-        'height': 300
+        'height': 300,
+        'type': 'auto'
     }
     if is_absolute(path):
         kwargs.update({
@@ -17,6 +24,12 @@ def generate_imaginary_url(path):
         kwargs.update({
             'file': path
         })
-    return 'http://127.0.0.1:9000/unsafe/crop?{0}'.format(
-        urlencode(kwargs, safe='/')
-    )
+
+    query = 'crop?{}'.format(urlencode(kwargs, safe='/'))
+    md5_digest = hashlib.md5('{}:{}'.format(query, SECRET).encode('utf-8')).digest()
+    key = base64.b64encode(md5_digest).decode('utf-8')
+    print(key, type(key))
+    # Make the key look like Nginx expects.
+    key = key.replace('+', '-').replace('/', '_').rstrip('=')
+
+    return 'http://127.0.0.1:9000/{}/{}'.format(key, query)

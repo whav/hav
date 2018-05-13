5091d9bc27588ec5f152c5e5d915874e9a106b0f
Sean Mc Allister <sean@gmx.org>
2018-05-13 22:06:40 +0200

    remove loading icons from start screen

--- frontend/src/admin/home.js
+++ frontend/src/admin/home.js
@@ -1,17 +1,15 @@
 /**
  * Created by sean on 01/02/17.
  */
-import React from 'react';
-import LoadingIndicator from './ui/loading'
+import React from "react";
 
 const Welcome = () => {
-    return <div>
-        <h1>HAV Dashboard</h1>
-        <p>
-            This will eventually be filled with dashboard content.
-        </p>
-        <LoadingIndicator />
+  return (
+    <div>
+      <h1>HAV Dashboard</h1>
+      <p>This will eventually be filled with dashboard content.</p>
     </div>
-}
+  );
+};
 
-export default Welcome
+export default Welcome;

7821ac1728d985337fd6e2ce50b63c74e17608ca
Sean Mc Allister <sean@gmx.org>
2018-07-22 22:51:42 +0200

    pass logger to image converter

--- backend/apps/webassets/operations/image.py
+++ backend/apps/webassets/operations/image.py
@@ -2,8 +2,10 @@ import os
 import shutil
 import requests
 from hav.utils.imaginary import generate_url
+import logging
 
-def convert(source, target, archivefile):
+
+def convert(source, target, archivefile, logger=logging.getLogger(__file__)):
     # since the source argument is absolute
     # grab the relative path from the database
     # object directly
@@ -15,8 +17,10 @@ def convert(source, target, archivefile):
         height=None,
         type='jpeg'
     )
+    logger.info('Grabbing image from %s.' % url)
 
     response = requests.get(url, stream=True)
+    response.raise_for_status()
 
     with open(target, 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)

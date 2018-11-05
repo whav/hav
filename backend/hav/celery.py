6aecac1764ff34c83abe04069eb6c8bce343a1a7
Sean Mc Allister <sean@gmx.org>
2018-11-05 09:13:27 +0100

    remove print from logging setup

--- backend/hav/celery.py
+++ backend/hav/celery.py
@@ -9,18 +9,16 @@ app = Celery('hav')
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 
 @signals.setup_logging.connect
 def setup_celery_logging(**kwargs):
-    print('Celery Logging...',kwargs)
+    # This disables any "optimizations" that celery does to the logging setup
     pass
 
-app.log.setup()
 
+app.log.setup()
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()

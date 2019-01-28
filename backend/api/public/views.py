f16081c59ea96b2f5d5d80b84ae9f3b4d45514f2
Sean Mc Allister <sean@gmx.org>
2019-01-28 21:05:18 +0100

    starting on public api

---
+++ backend/api/public/views.py
@@ -0,0 +1,20 @@
+from rest_framework.generics import ListAPIView
+from rest_framework import serializers
+from apps.hav_collections.models import Collection
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = [
+            'name',
+            'short_name',
+            'slug'
+        ]
+
+class CollectionListView(ListAPIView):
+    serializer_class = CollectionSerializer
+
+    def get_queryset(self):
+        return Collection.objects.all()
+

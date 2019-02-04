fe7af39016a2a7d95042826f6d75ef8e620a8137
Sean Mc Allister <sean@gmx.org>
2019-02-04 23:56:28 +0100

    add srcset to schema

--- backend/apps/media/schema.py
+++ backend/apps/media/schema.py
@@ -5,13 +5,30 @@ from graphene_django.types import DjangoObjectType
 from .models import Media, MediaCreator, License
 from apps.sets.schema import SetType
 
-from hav_utils.imaginary import generate_thumbnail_url
+from hav_utils.imaginary import generate_thumbnail_url, generate_srcset_urls
 
 
 class MediaType(DjangoObjectType):
 
     ancestors = graphene.List(SetType)
     thumbnail_url = graphene.Field(graphene.String)
+    srcset =graphene.List(graphene.String)
+
+    def resolve_srcset(self, info):
+        if self.primary_file:
+            webasset_images = filter(
+                lambda x: x.mime_type.startswith('image'),
+                self.primary_file.webasset_set.all()
+            )
+            try:
+                webasset = list(webasset_images)[0]
+            except IndexError:
+                pass
+            else:
+                return [f'{src[1]} {src[0]}w' for src in generate_srcset_urls(webasset)]
+        return []
+
+
 
     def resolve_thumbnail_url(self, info):
         if self.primary_file:

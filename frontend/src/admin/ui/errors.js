2ce009bbe79a3eff631e647e08ea6553fb257c47
Sean Mc Allister <sean@gmx.org>
2019-02-21 11:20:57 +0100

    make directories unselectable for ingest

--- frontend/src/admin/ui/errors.js
+++ frontend/src/admin/ui/errors.js
@@ -10,7 +10,6 @@ class ErrorBoundary extends React.Component {
   // }
 
   static getDerivedStateFromError(error) {
-    console.error(error);
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }

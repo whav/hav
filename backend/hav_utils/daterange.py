8242c9ae41551e071a6d779aeb1f5e8300cfd4aa
Sean Mc Allister <sean@gmx.org>
2019-02-26 21:29:37 +0100

    daterange utils

---
+++ backend/hav_utils/daterange.py
@@ -0,0 +1,63 @@
+import aniso8601 as iso8601
+from aniso8601.resolution import DateResolution, TimeResolution
+from datetime import datetime, time
+import calendar
+import re
+
+split_date_time = re.compile(r"(?P<date>[\d\-]+)[\ T](?P<time>.*)")
+
+
+def parse(dt_string):
+    d = split_date_time.match(dt_string).groupdict()
+    date_part = d.get("date")
+    time_part = d.get("time")
+    start, end = parse_date(date_part)
+    if time_part:
+        d = start.date
+        tMin, tMax = parse_time(time_part)
+        start, end = datetime.combine(d, tMin), datetime.combine(d, t.Max)
+
+    return start, end
+
+
+def parse_time(time_string):
+    t, resolution = (
+        iso8601.parse_time(time_string),
+        iso8601.get_time_resolution(time_string),
+    )
+    tMin = t
+    if resolution == TimeResolution.Hours:
+        tMax = t.replace(
+            minute=time.max.minute,
+            second=time.max.second,
+            microsecond=time.max.microsecond,
+        )
+    elif resolution == TimeResolution.Minutes:
+        tMax = t.replace(second=time.max.second, microsecond=time.max.microsecond)
+    elif resolution == TimeResolution.Seconds:
+        if t.microsecond:
+            tMax = t
+        else:
+            tMax = t.replace(microsecond=time.max.microsecond)
+    else:
+        raise NotImplementedError(f'Unable to deal with time input "{time_string}".')
+    return tMin, tMax
+
+
+def parse_date(date_string):
+    d, resolution = (
+        iso8601.parse_date(date_string),
+        iso8601.get_date_resolution(date_string),
+    )
+    dMin = datetime.combine(d, time.min)
+    if resolution == DateResolution.Year:
+        dMax = d.replace(month=12, day=31)
+    elif resolution == DateResolution.Month:
+        lastDayOfMonth = calendar.monthrange(d.year, d.month)[1]
+        dMax = d.replace(day=lastDayOfMonth)
+    elif resolution == DateResolution.Day:
+        dMax = d
+    else:
+        raise NotImplementedError(f'Unable to deal with input "{dt_string}".')
+    dMax = datetime.combine(dMax, time.max)
+    return dMin, dMax
---
+++ backend/hav_utils/tests/test_daterange.py
@@ -0,0 +1,63 @@
+import unittest
+from ..daterange import parse, parse_date, parse_time
+from datetime import date, datetime, time
+
+
+class TestDateString(unittest.TestCase):
+    def test_year(self):
+        self.assertEqual(
+            parse_date("2008"),
+            (
+                datetime.combine(date(2008, 1, 1), time.min),
+                datetime.combine(date(2008, 12, 31), time.max),
+            ),
+        )
+
+    def test_month(self):
+        self.assertEqual(
+            parse_date("2008-05"),
+            (
+                datetime.combine(date(2008, 5, 1), time.min),
+                datetime.combine(date(2008, 5, 31), time.max),
+            ),
+        )
+
+        self.assertEqual(
+            parse_date("2020-02"),
+            (
+                datetime.combine(date(2020, 2, 1), time.min),
+                datetime.combine(date(2020, 2, 29), time.max),
+            ),
+            "deal with leap years",
+        )
+
+    def test_day(self):
+        day = date(2019, 2, 26)
+        self.assertEqual(
+            parse_date("2019-02-26"),
+            (datetime.combine(day, time.min), datetime.combine(day, time.max)),
+        )
+
+
+class TestTimeParsing(unittest.TestCase):
+    def test_minute(self):
+        self.assertEqual(
+            parse_time("14:01"), (time(14, 1), time.max.replace(hour=14, minute=1))
+        )
+
+    def test_hour(self):
+        self.assertEqual(parse_time("14"), (time(14, 0), time.max.replace(hour=14)))
+
+    def test_second(self):
+        self.assertEqual(
+            parse_time("14:03:22"),
+            (time(14, 3, 22), time.max.replace(hour=14, minute=3, second=22)),
+        )
+
+    def test_microseconds(self):
+        t = time(14, 42, 5, 123000)
+        self.assertEqual(parse_time("14:42:05.123"), (t, t))
+
+
+if __name__ == "__main__":
+    unittest.main()

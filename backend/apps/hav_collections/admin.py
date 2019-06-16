f47572bc528b18fcb1a78ea8fa04da931350c669
Sean Mc Allister <sean@gmx.org>
2019-06-16 14:11:55 +0200

    add media count to collection admin page

--- backend/apps/hav_collections/admin.py
+++ backend/apps/hav_collections/admin.py
@@ -3,7 +3,10 @@ from .models import Collection
 
 
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'short_name', 'slug', 'public')
+    list_display = ('name', 'short_name', 'slug', 'media_count', 'public')
+
+    def media_count(self, obj):
+        return obj.media_set.count()
 
 
 admin.site.register(Collection, CollectionAdmin)

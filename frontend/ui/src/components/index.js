27e0bb7d330a5b83c7d2be0f6c98d0f31b4e82bc
Sean Mc Allister <sean@gmx.org>
2020-08-30 22:59:13 +0200

    restore and update ui

--- frontend/ui/src/components/index.js
+++ frontend/ui/src/components/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Heading as Title } from "@theme-ui/components";
 
+export * from "@theme-ui/components";
+
 export const H1 = ({ children, ...props }) => <Title>{children}</Title>;
 export const H2 = ({ children, ...props }) => (
   <Title as="h2" {...props}>

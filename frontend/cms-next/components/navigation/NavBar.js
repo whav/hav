c114ccdfb9a2157773b5011f6f6f86d72a7f3430
Sean Mc Allister <sean@gmx.org>
2020-09-07 23:59:10 +0200

    simple nav component

--- frontend/cms-next/components/navigation/NavBar.js
+++ frontend/cms-next/components/navigation/NavBar.js
@@ -6,28 +6,63 @@ import Link from "next/link";
 
 const NavBar = () => {
   const [navVisible, setNavVisibility] = useState(false);
-  console.log("visible?", navVisible);
   return (
-    <div className={styles.navbar}>
-      <div className={styles.nav_main}>
-        <div className={styles.nav_image_main}>
+    <div className={styles.navbar_wrapper}>
+      <div className={styles.navbar}>
+        <div className={styles.navbar_banner}>
           <Link href="/">
             <a>
               <img src="/logos/hav.svg" />
             </a>
           </Link>
         </div>
-        <nav className={navVisible ? styles.nav : styles.nav_hidden}>
+        <div className={styles.navbar_hamburger}>
+          <MenuButton onClick={() => setNavVisibility(!navVisible)} />
+        </div>
+      </div>
+      <nav className={`${styles.nav} ${navVisible ? "" : styles.nav_hidden}`}>
+        <ul>
+          <li>
+            <Link href="/one/">
+              <a>one</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/two/">
+              <a>Two</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/three/">
+              <a>three</a>
+            </Link>
+          </li>
+        </ul>
+        <div className="nav-bottom">
           <ul>
-            <li>one</li>
-            <li>two</li>
-            <li>three</li>
+            <li>
+              <img src="/logos/cirdis.svg" />
+            </li>
+
+            <li>
+              <img src="/logos/univie.svg" />
+            </li>
+            <li>
+              <Link href="/imprint/">
+                <a>Imprint</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/open-knowledge/">
+                <a>License Models</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/cooperation/">How to cooperate</Link>
+            </li>
           </ul>
-        </nav>
-      </div>
-      <div className={styles.nav_aside}>
-        <MenuButton onClick={() => setNavVisibility(!navVisible)} />
-      </div>
+        </div>
+      </nav>
     </div>
   );
 };

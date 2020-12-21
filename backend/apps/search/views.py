1e08580486fae41a929d602a19dbac4fc5f5286c
Sean Mc Allister <sean@gmx.org>
2020-12-21 08:13:09 +0100

    SearchAPI view improvements

--- backend/apps/search/views.py
+++ backend/apps/search/views.py
@@ -5,6 +5,10 @@ from rest_framework import serializers
 from apps.media.models import Media
 
 
+class QuerySerializer(serializers.Serializer):
+    query = serializers.CharField(required=True)
+
+
 class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Media
@@ -13,8 +17,9 @@ class MediaSerializer(serializers.ModelSerializer):
 
 class SearchView(APIView):
     def get(self, request, format=None):
-        query = request.GET.get('query', '')
-        print(query)
+        query_serializer = QuerySerializer(data=request.GET)
+        query_serializer.is_valid()
+        query = query_serializer.data.get('query')
         # TODO: stuff like this:
         from django.contrib.postgres.search import SearchVector
         media_hits  = Media.objects.annotate(search=SearchVector('title', 'description', 'collection__name', 'set__name')).filter(
@@ -24,3 +29,5 @@ class SearchView(APIView):
             'count': 0,
             'results': media_serializer.data
         })
+
+

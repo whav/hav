660f48fe3b6178bfec09797af0149e9b1cba08e1
Sean Mc Allister <sean@gmx.org>
2021-01-18 23:33:29 +0100

    attempt to convert dng files

--- backend/apps/webassets/operations/image.py
+++ backend/apps/webassets/operations/image.py
@@ -1,13 +1,40 @@
+from tempfile import NamedTemporaryFile
 import pyvips
 import logging
+import mimetypes
+import imageio
+import rawpy
 
 logger = logging.getLogger(__name__)
 
+raw_formats = set([
+    "image/x-nikon-nef",
+    "image/x-adobe-dng"
+])
+
 
 def convert(source, target, *args, **kwargs):
-    logger.debug(f"Image convert called with source {source}, target {target}.")
-    image = pyvips.Image.new_from_file(source)
-    image.write_to_file(target)
+    logger.debug(f"Image convert called with source {source} ({mimetypes.guess_type(source)[0]}), target {target}.")
+
+    tmp_file = None
+
+    try:
+        if mimetypes.guess_type(source)[0] in raw_formats:
+            with rawpy.imread(source) as raw:
+                rgb = raw.postprocess()
+
+            tmp_file = NamedTemporaryFile(suffix='tiff')
+            imageio.imsave(tmp_file, rgb, format='tiff')
+            tmp_file.seek(0)
+            logger.debug(f"Image convert created temp file at {tmp_file.name}.")
+            source = tmp_file.name
+
+        # actually do the conversion
+        image = pyvips.Image.new_from_file(source)
+        image.write_to_file(target)
+    finally:
+        if tmp_file:
+            tmp_file.close()
 
 
 convert.extension = ".jpg"

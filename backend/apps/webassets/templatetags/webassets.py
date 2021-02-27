b98a85d2d03dff0a3c2a381df4c76d7a814b67c7
Sean Mc Allister <sean@gmx.org>
2021-02-27 14:06:47 +0100

    webasset and archive preview tags

---
+++ backend/apps/webassets/templatetags/webassets.py
@@ -0,0 +1,33 @@
+from django import template
+from ..models import WebAsset
+from ...archive.models import ArchiveFile
+
+register = template.Library()
+
+template_base = 'webassets/tags'
+
+@register.inclusion_tag(f'{template_base}/preview.html')
+def webasset_preview(webasset: WebAsset):
+    media_type = webasset.mime_type.split('/')[0]
+    context = {
+        'webasset': webasset,
+        'preview_template': None,
+        'poster': None,
+    }
+    if media_type == 'image':
+        context.update({
+            'preview_template': f'{template_base}/image.html'
+        })
+    if media_type in ['audio', 'video']:
+        context.update({
+            'preview_template': f'{template_base}/video.html'
+        })
+    # TODO: handling for other media types
+    return context
+
+
+@register.inclusion_tag(f'{template_base}/archive.html')
+def archive_file_preview(archive_file: ArchiveFile):
+    return {
+        'archive_file': archive_file
+    }

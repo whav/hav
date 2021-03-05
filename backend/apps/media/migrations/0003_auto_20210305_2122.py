e1cdf695b0a6339796f75d92dd41101e2b7d52e9
Sean Mc Allister <sean@gmx.org>
2021-03-05 22:26:25 +0100

    do not require modified_by

--- backend/apps/media/migrations/0003_auto_20210305_2122.py
+++ backend/apps/media/migrations/0003_auto_20210305_2122.py
@@ -9,13 +9,19 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('media', '0002_auto_20210106_2044'),
+        ("media", "0002_auto_20210106_2044"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='media',
-            name='modified_by',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, related_name='modified_media', to=settings.AUTH_USER_MODEL),
+            model_name="media",
+            name="modified_by",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="modified_media",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]

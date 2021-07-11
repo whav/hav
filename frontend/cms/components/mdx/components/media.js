75490f42b6f57839d80fc74192f993c61ef2064a
Jürgen Schörflinger <bearsoundandfire@gmail.com>
2021-07-12 00:20:22 +0900

    add img size-hints to mdx-media-component

--- frontend/cms/components/mdx/components/media.js
+++ frontend/cms/components/mdx/components/media.js
@@ -1,21 +1,22 @@
 import { useAPI } from "hooks";
 
-const ImageMedia = ({ media, caption, title, url }) => {
+const ImageMedia = ({ media, sizes }) => {
   return (
     <img
       alt={media.title}
+      sizes={sizes}
       src={media.thumbnailUrl}
       srcSet={media.srcset.join(", ")}
     />
   );
 };
 
-const MediaSwitch = (props) => {
-  return <ImageMedia {...props} />;
+const MediaSwitch = (data, props) => {
+  return <ImageMedia {...data} {...props.sizes} />;
 };
 
 const Media = (props) => {
-  const { id, caption } = props;
+  const { id, caption, sizes } = props;
   const { data } = useAPI("/api/media/", { mediaId: id });
   if (!data) {
     return null;
@@ -24,7 +25,7 @@ const Media = (props) => {
   return (
       <div className="max-w-sm m-4">
         {caption && <div>{caption}</div>}
-        <MediaSwitch {...data} />
+        <MediaSwitch {...data} {...props} />
       </div>
   );
 };

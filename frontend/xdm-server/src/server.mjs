e721ecd685f4da996d7b2ea33b5920b914e2c3e1
Sean Mc Allister <sean@gmx.org>
2021-08-08 21:59:55 +0200

    starting on mdx render server

---
+++ frontend/xdm-server/src/server.mjs
@@ -0,0 +1,29 @@
+import bundleMDX from "./mdx.mjs";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const dummyContent = `
+# Wahoo
+
+Pure markdown
+
+`.trim();
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get("/", (req, res) => {
+  res.send("Don't get me!");
+});
+
+app.post("/", async (req, resp) => {
+  const code = await bundleMDX(req.body.mdx || dummyContent);
+  resp.type(".js");
+  resp.send(code);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

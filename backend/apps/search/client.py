e8b1d3b2335c6a340d666c025a2e9952bce84a2b
Sean Mc Allister <sean@gmx.org>
2021-10-26 22:37:08 +0200

    search improvements and simple pagination

--- backend/apps/search/client.py
+++ backend/apps/search/client.py
@@ -17,7 +17,7 @@ def get_index():
     return get_client().index(index)
 
 
-def search(query, node=None, offset=0):
+def search(query, node=None, limit=20, offset=0):
     search_filters = ""
 
     if node:
@@ -30,47 +30,47 @@ def search(query, node=None, offset=0):
         "cropLength": 200,
         "attributesToHighlight": ["*"],
         "offset": offset,
-        "limit": 20,
+        "limit": limit,
     }
     if search_filters:
         search_options.update({"filter": search_filters})
 
-        index = get_index()
-        response = index.search(query, search_options)
-        hits = response.get("hits", [])
+    index = get_index()
+    response = index.search(query, search_options)
+    hits = response.get("hits", [])
 
-        # get all folder and media pks in one go
-        media_pks = []
-        node_pks = []
-        for hit in hits:
-            type = hit.get("type")
-            pk = hit.get("pk")
-            if type == "media":
-                media_pks.append(pk)
-            elif type == "folder":
-                node_pks.append(pk)
-            else:
-                raise NotImplementedError(
-                    f'Unknown type "{type}" returned from search query.'
-                )
+    # get all folder and media pks in one go
+    media_pks = []
+    node_pks = []
+    for hit in hits:
+        type = hit.get("type")
+        pk = hit.get("pk")
+        if type == "media":
+            media_pks.append(pk)
+        elif type == "folder":
+            node_pks.append(pk)
+        else:
+            raise NotImplementedError(
+                f'Unknown type "{type}" returned from search query.'
+            )
 
-        # and fetch in one query per type
-        media_items = {
-            media.pk: media
-            for media in Media.objects.filter(pk__in=media_pks)
-            .prefetch_related("collection")
-            .prefetch_related("files__webasset_set", "collection")
-        }
-        nodes = {node.pk: node for node in Node.objects.filter(pk__in=node_pks)}
+    # and fetch in one query per type
+    media_items = {
+        media.pk: media
+        for media in Media.objects.filter(pk__in=media_pks)
+        .prefetch_related("collection")
+        .prefetch_related("files__webasset_set", "collection")
+    }
+    nodes = {node.pk: node for node in Node.objects.filter(pk__in=node_pks)}
 
-        for hit in hits:
-            type = hit.get("type")
-            pk = hit.get("pk")
-            if type == "media":
-                m = media_items[pk]
-            elif type == "folder":
-                m = nodes[pk].get_representative_media()
+    for hit in hits:
+        type = hit.get("type")
+        pk = hit.get("pk")
+        if type == "media":
+            m = media_items[pk]
+        elif type == "folder":
+            m = nodes[pk].get_representative_media()
 
-            hit.update({"object": m})
+        hit.update({"object": m})
 
-        return response
+    return response

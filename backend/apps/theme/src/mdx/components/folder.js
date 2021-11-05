f53d27ef952223bec4f9f9f4dc87dc1bae1714f9
Sean Mc Allister <sean@gmx.org>
2021-11-05 23:05:27 +0100

    Node mdx components

--- backend/apps/theme/src/mdx/components/folder.js
+++ backend/apps/theme/src/mdx/components/folder.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React from "react";
+import useSWR from "swr";
+import fetcher from "../utils/fetcher";
 
-const Folder = (props) => {
-  const { id } = props;
+import { MediaPreview } from "./media";
 
-  // const { data } = useAPI("/api/sets/", { set: id });
-  // if (!data) {
-  //   return null;
-  // }
-    return <div>FOLDER</div>;
-  // const {
-  //   name,
-  //   collection: { slug },
-  // } = data;
-  // return <Link href={`/collections/${slug}/browse/${id}/`}>{data.name}</Link>;
+const Node = (props) => {
+  const { id, media_id, caption } = props;
+
+  const { data, error } = useSWR(`/api/public/node/${id}/`, fetcher);
+
+  if (!data) {
+    return null;
+  }
+
+  if (error) {
+    <div>failed to load</div>;
+  }
+
+  // grab defaults from data
+  let preview_media_id = media_id || data.representative_media_id;
+  let title = caption || data.name || "";
+  let url = data.url;
+
+  return (
+    <a href={url}>
+      <div className="tile">
+        <div className="preview">
+          {preview_media_id && <MediaPreview id={preview_media_id} />}
+        </div>
+      </div>
+      <div className={"caption"}>{title}</div>
+    </a>
+  );
 };
 
-export default Folder;
+export default Node;
--- backend/apps/theme/src/mdx/components/media.js
+++ backend/apps/theme/src/mdx/components/media.js
@@ -2,44 +2,64 @@ import React from "react";
 import useSWR from "swr";
 import fetcher from "../utils/fetcher";
 
-const ImageMedia = ({ title, thumbnail, aspect_ratio, srcset=[] }) => {
-    let srcset_attr = '';
-    if (srcset.length) {
-        srcset_attr = srcset.map(([width, url]) => `${url} ${width}w`).join(', ')
-    }
-    // let width = 'auto';
-    // if (aspect_ratio) {
-    //     width = Math.floor(
-    //         Math.sqrt(4800 * 6 * aspect_ratio)
-    //     )
-    //     width = `${width}px`
-    // }
-    return <img className={"preview-image"} alt={title} src={thumbnail} srcSet={srcset_attr} sizes="300px" />;
+const ImageMedia = ({ title, thumbnail, aspect_ratio, srcset = [] }) => {
+  let srcset_attr = "";
+  if (srcset.length) {
+    srcset_attr = srcset.map(([width, url]) => `${url} ${width}w`).join(", ");
+  }
+
+  return (
+    <img
+      className={"preview-image"}
+      alt={title}
+      src={thumbnail}
+      srcSet={srcset_attr}
+      sizes="300px"
+    />
+  );
 };
 
 const MediaSwitch = (props) => {
   return <ImageMedia {...props} />;
 };
 
+const MediaPreview = (props) => {
+  const { id } = props;
+  const { data, error } = useSWR(`/api/public/media/${id}/`, fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return <MediaSwitch {...data} />;
+};
+
 const Media = (props) => {
-  const { id, caption, sizes, width, link_to_parent_node, link_to_node_with_id } = props;
+  const { id, caption, link_to_parent_node, link_to_node_with_id } = props;
   const { data, error } = useSWR(`/api/public/media/${id}/`, fetcher);
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
   // return <ImageMedia {...data} />;
 
-    return (
+  return (
     // ugly...
-    <a href={link_to_node_with_id ? data.set_url.replace(/\d+\/$/, link_to_node_with_id + '/') : link_to_parent_node ? data.set_url : data.url}>
-        <div className="tile">
-            <div className="preview">
-                <MediaSwitch {...data} {...props} />
-            </div>
-            <div className={"caption"}>{caption}</div>
+    <a
+      href={
+        link_to_node_with_id
+          ? data.set_url.replace(/\d+\/$/, link_to_node_with_id + "/")
+          : link_to_parent_node
+          ? data.set_url
+          : data.url
+      }
+    >
+      <div className="tile">
+        <div className="preview">
+          <MediaSwitch {...data} {...props} />
         </div>
+        <div className={"caption"}>{caption}</div>
+      </div>
     </a>
   );
 };
 
 export default Media;
+
+export { MediaSwitch, MediaPreview };

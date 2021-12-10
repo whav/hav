5e0f5c5233af21a3c810a5a8312549d62bdeeee8
Jürgen Schörflinger <bearsoundandfire@gmail.com>
2021-12-10 15:51:28 +0900

    display license note for media with non-open licenses (TODO: create a simple contact form for publication requests)

---
+++ backend/apps/media/templatetags/license.py
@@ -0,0 +1,22 @@
+from django import template
+from apps.media.models import License
+from django.db.models import Q
+
+register = template.Library()
+
+licenses_non_open = License.objects.filter(Q(short_name='c/p') |
+                                           Q(short_name__startswith='ccbynd') |
+                                           Q(short_name__startswith='ccbyncnd')
+                                           )
+
+note_text = """This media could not be provided under a license conforming to open research data standards. The HAV does not serve as mediator between the copyright holder and interested third-parties.
+Please contact the media creator or copyright-holder if you need to obtain additional rights."""
+
+
+@register.inclusion_tag("ui/components/license.html")
+def full_license_info(license: License):
+    assert isinstance(license, License)
+
+    license_note = note_text if license in licenses_non_open else None
+
+    return {'license': license, 'license_note': license_note}

318b4a7e0388c6227b916c1b55fe14c85c2e31fb
Sean Mc Allister <sean@gmx.org>
2022-02-07 22:21:25 +0100

    permissions

--- src/hav/apps/accounts/models.py
+++ src/hav/apps/accounts/models.py
@@ -1,7 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from apps.media.models import Media
 
 class User(AbstractUser):
     class Meta:
         db_table = "auth_user"
+
+    def can_view_media(self, media: Media):
+        from .permissions import can_view_media
+        return can_view_media(self, media)
---
+++ src/hav/apps/accounts/permissions.py
@@ -0,0 +1,32 @@
+from datetime import date
+from functools import lru_cache
+
+from .models import User
+from apps.media.models import Media
+from apps.webassets.models import WebAsset
+from apps.hav_collections.models import Collection
+
+
+@lru_cache(maxsize=1, typed=True)
+def can_view_webassets(user: User, webasset: WebAsset):
+    media = webasset.archivefile.media
+    return can_view_media(user, media)
+
+
+@lru_cache(maxsize=1, typed=True)
+def is_collection_admin(user: User, collection: Collection):
+    if user.is_authenticated:
+        return user in collection.administrators.all()
+    return False
+
+
+def can_view_media(user: User, media: Media):
+    has_active_embargo = (
+        media.embargo_end_date and media.embargo_end_date >= date.today()
+    )
+    is_private = media.is_private
+
+    if is_private or has_active_embargo:
+        return is_collection_admin(user, media.collection)
+
+    return True

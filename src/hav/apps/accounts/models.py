854845090c23eaad8fa86a820d2b19fadd108cde
Sean Mc Allister <sean@gmx.org>
2022-02-07 22:58:11 +0100

    fix circular import by removing typings

--- src/hav/apps/accounts/models.py
+++ src/hav/apps/accounts/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
-from apps.media.models import Media
+
 
 class User(AbstractUser):
     class Meta:
         db_table = "auth_user"
 
-    def can_view_media(self, media: Media):
+    def can_view_media(self, media):
         from .permissions import can_view_media
+
         return can_view_media(self, media)

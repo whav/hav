b594a79ec56f1ddfb936a4b7508da7cfef34f303
Sean Mc Allister <sean@gmx.org>
2022-02-18 23:24:33 +0100

    more fixtures and tests for gallery views

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,48 +9,41 @@ from apps.media.models import Media, MediaType
 from apps.hav_collections.models import Collection
 from apps.sets.models import Node
 from apps.webassets.operations import create_webassets
+from apps.webassets.models import WebAsset
 
 
 @pytest.fixture
 def collection(user):
-
-    name = f"Collection:{str(uuid4())}"[:30]
+    slug = str(uuid4())
+    name = f"Collection:{slug}"[:30]
     root_node = Node.add_root(name=name, description=f"{name} - Root Node")
     collection = Collection.objects.create(
-        type=1, name=name, short_name=name, root_node=root_node
+        type=1, name=name, short_name=name, root_node=root_node, slug=slug
     )
     collection.administrators.set([user])
     return collection
 
 
 @pytest.fixture
-def archive_file(user):
-    path = Path(__file__).parent / "webassets/testdata/image.jpg"
-    assert path.exists()
-    af = ArchiveFile(created_by=user)
-    with path.open("rb") as f:
-        af.file.save(path.name, File(f))
+def create_archive_file(user):
+    created_files = []
 
-    af.save()
-    create_webassets(af.pk)
-    return af
+    def _create_af():
+        path = Path(__file__).parent / "webassets/testdata/image.jpg"
+        assert path.exists()
+        af = ArchiveFile(created_by=user)
+        with path.open("rb") as f:
+            af.file.save(path.name, File(f))
 
+        af.save()
+        webassets = create_webassets(af.pk)
+        created_files.extend(webassets)
+        created_files.append(af)
+        return af
 
-@pytest.fixture
-def media(collection, archive_file):
-    name = f"Media:{uuid4()}"
-
-    media_type = MediaType.objects.create(type=MediaType.DIGITAL, name=f"TestMediaType")
-    media = Media.objects.create(
-        title=name,
-        description=f"{name} - description",
-        collection=collection,
-        original_media_type=media_type,
-        set=collection.root_node,
-        created_by=collection.administrators.get(),
-    )
-    media.files.set([archive_file])
-    return media
+    yield _create_af
+    for some_object in created_files:
+        some_object.file.storage.delete(some_object.file.path)
 
 
 @pytest.fixture
@@ -67,3 +60,35 @@ def create_user(django_user_model):
 @pytest.fixture
 def user(create_user):
     return create_user()
+
+
+@pytest.fixture
+def create_media(collection, create_archive_file):
+    def _create_media(**kwargs):
+        archive_file = create_archive_file()
+        name = f"Media:{uuid4()}"
+        media_type, _ = MediaType.objects.get_or_create(
+            type=MediaType.DIGITAL, name=f"TestMediaType"
+        )
+        media = Media.objects.create(
+            title=name,
+            description=f"{name} - description",
+            collection=collection,
+            original_media_type=media_type,
+            set=kwargs.get("set", collection.root_node),
+            created_by=collection.administrators.get(),
+        )
+        media.files.set([archive_file])
+        return media
+
+    return _create_media
+
+
+@pytest.fixture
+def media(create_media):
+    return create_media()
+
+
+@pytest.fixture
+def archive_file(create_archive_file):
+    return create_archive_file()

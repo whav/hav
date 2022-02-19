9f9cad1ee23b6d0533d78c9cac4c043e639edb52
Sean Mc Allister <sean@gmx.org>
2022-02-19 09:49:24 +0100

    basic node editing view

--- src/hav/conf/ui_urls.py
+++ src/hav/conf/ui_urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from hav.views.public import CollectionRoot, FolderView, MediaView, SearchView
 from hav.views.public.flatpages import FlatpageView
+from hav.views.crud.folder import FolderUpdateView
 
 urlpatterns = [
     path("", FlatpageView.as_view(slug="index"), name="landing_page"),
@@ -10,7 +11,18 @@ urlpatterns = [
         include(
             [
                 path("", CollectionRoot.as_view(), name="collection_root"),
-                path("node/<int:node_pk>/", FolderView.as_view(), name="folder_view"),
+                # path("node/<int:node_pk>/", FolderView.as_view(), name="folder_view"),
+                path(
+                    "node/<int:node_pk>/",
+                    include(
+                        [
+                            path("", FolderView.as_view(), name="folder_view"),
+                            path(
+                                "edit/", FolderUpdateView.as_view(), name="folder_edit"
+                            ),
+                        ]
+                    ),
+                ),
                 path("media/<int:media_pk>/", MediaView.as_view(), name="media_view"),
                 path("search/", SearchView.as_view(), name="collection_search"),
             ]

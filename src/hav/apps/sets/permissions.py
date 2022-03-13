a532c21002ce5667c43c91714b239db461b6efa5
Sean Mc Allister <sean@gmx.org>
2022-03-13 18:44:49 +0100

    move collection admin logic to the model

--- src/hav/apps/sets/permissions.py
+++ src/hav/apps/sets/permissions.py
@@ -1,8 +1,7 @@
 from .models import Node
-from hav.apps.accounts.permissions import is_collection_admin
 from hav.apps.accounts.models import User
 
 
 def can_edit_node(user: User, node: Node):
     collection = node.get_collection()
-    return is_collection_admin(user, collection)
+    return collection.is_admin(user)

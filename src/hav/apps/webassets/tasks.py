5beb753a82bccc524a1bf62abdd345662ebd1f41
Jürgen Schörflinger <bearsoundandfire@gmail.com>
2022-04-28 15:11:15 +0900

    up webasset creation timout for rq and add as env to settings

--- src/hav/apps/webassets/tasks.py
+++ src/hav/apps/webassets/tasks.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 
+from django.conf import settings
 from django_rq import get_queue, job
 from rq import get_current_job
 
@@ -10,13 +11,13 @@ from .operations import create_webassets
 logger = logging.getLogger(__name__)
 
 
-@job("webassets")
+@job("webassets", timeout=settings.WEBASSET_CREATION_TIMEOUT)
 def create(archived_file_id):
     webassets = create_webassets(archived_file_id)
     return [wa.pk for wa in webassets]
 
 
-@job("webassets")
+@job("webassets", timeout=settings.WEBASSET_CREATION_TIMEOUT)
 def create_webassets_after_archive_task():
     current_job = get_current_job()
     previous_job_id = current_job.dependency.id

8ce145bf3d89285ae7f8e1be58603208de68f849
Jürgen Schörflinger <bearsoundandfire@gmail.com>
2023-10-05 14:38:19 +0900

    add gps-coords and maplinks to media view

--- src/hav/apps/media/templatetags/media.py
+++ src/hav/apps/media/templatetags/media.py
@@ -2,6 +2,7 @@ from logging import getLogger
 
 from django import template
 from django.template.defaultfilters import date as django_date_filter
+from django.utils.safestring import mark_safe
 from psycopg2.extras import DateTimeTZRange
 
 from hav.utils.daterange import Resolutions, ReverseDateTimeRange
@@ -26,4 +27,14 @@ def daterange(dt_range: DateTimeTZRange):
 
     return (
         f"{django_date_filter(dt_range.lower)} - {django_date_filter(dt_range.upper)}"
+    )
+
+
+@register.simple_tag()
+def maplink(lat: float, lon: float):
+    # uri_base = "geo:"
+    geo_uri = "https://www.openstreetmap.org/?mlat={lat}&mlon={lon}#map10/{lat}/{lon}&layers=H"
+    return mark_safe(
+        f"<a href='{geo_uri.format(lat=lat, lon=lon)}' target='_blank'>{round(lat,4)},\
+        {round(lon,4)}</a>"
     )
